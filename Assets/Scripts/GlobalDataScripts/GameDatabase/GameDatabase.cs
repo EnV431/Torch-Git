20fbfe3d0cdfb423a9628b899611587fb697518e
EnV431 <shollandat@gmail.com>
2026-07-26 15:42:58 +0930

    setup the resource scriptable object and list

---
+++ Assets/Scripts/GlobalDataScripts/GameDatabase/GameDatabase.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+public class GameDatabase : MonoBehaviour 
+{
+    private static GameDatabase GameDatabaseInstance { get; set; }
+
+    [SerializeField]private List<ResourceData> resourceDataList;
+
+
+
+
+   public IReadOnlyList<ResourceData> ResourceDataList => resourceDataList; //makes it a public readonly
+    //Public ItemDataList,
+    //Public CharacterDataList,
+   // Public EventDataList
+
+
+
+    private void Awake()
+    {
+        #region SingletonSetup
+        DontDestroyOnLoad(gameObject);
+        if (GameDatabaseInstance != null && GameDatabaseInstance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        GameDatabaseInstance = this;
+        #endregion
+    }
+
+
+
+}
---
+++ Assets/Scripts/ScriptableObjects/Resources/ResourceData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResourceData", menuName = "Scriptable Objects/ResourceData")]
+public class ResourceData : ScriptableObject
+{
+    [Header("Identifiers")]
+    public int resourceId;
+    public string resourceName;
+
+
+
+}
