be2b64085640b48cf3bdd0e20c723c5a16bdd3d0
EnV431 <shollandat@gmail.com>
2026-07-26 16:33:58 +0930

    add resource script

--- Assets/Scripts/MainScripts/DataContainer/Resources/Resource.cs
+++ Assets/Scripts/MainScripts/DataContainer/Resources/Resource.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
-public class Resource
+public class Resource : MonoBehaviour
 {
-    
+    [SerializeField] ResourceData resourceData;
+
+    private int value;
+    public int Value => value;
+
 }
