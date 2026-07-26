7431fd97403d6ad53cf28211c97611ae4e433777
EnV431 <shollandat@gmail.com>
2026-07-26 14:58:11 +0930

    add the GameManager scripts

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager GameManagerInstance { get; set; }
+
+    private Dictionary<Type, IGameModule> gameModulesDictionary = new Dictionary<Type, IGameModule>();
+    [SerializeField] private List<IGameModule> gameModuleList = new();
+    private void Awake()
     {
-        
+        #region SingletonSetup
+        DontDestroyOnLoad(gameObject);
+        if (GameManagerInstance != null && GameManagerInstance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        GameManagerInstance = this;
+        #endregion
+
+        #region LoadModulesToDictionary
+        gameModulesDictionary.Clear();
+        gameModuleList.AddRange(GetComponents<IGameModule>());
+        #endregion
+
+
+        foreach (var module in gameModuleList)
+        {
+            gameModulesDictionary.Add(module.GetType(), module); //add module to dict
+            Debug.Log(module + " added to dict");
+            module.InitializeModule(); //inits module
+        }
+    }
+    public T GetModule<T>() where T : class, IGameModule //public lookup method
+    {
+
+        if (gameModulesDictionary.TryGetValue(typeof(T), out var module))
+        {
+            return module as T;
+        }
+        else Debug.Log(" Game Module not found in Dictionary"); return null;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
