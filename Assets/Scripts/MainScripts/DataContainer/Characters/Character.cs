92ebae141743b92af4d37546d28064900c5ba954
EnV431 <shollandat@gmail.com>
2026-08-02 22:11:17 +0930

    moved all character data into class

--- Assets/Scripts/MainScripts/DataContainer/Characters/Character.cs
+++ Assets/Scripts/MainScripts/DataContainer/Characters/Character.cs
@@ -8,16 +8,29 @@ public class Character
     public CharacterAttributes CharacterAttributes { get; private set; }
     public CharacterStats CharacterStats { get; private set; }
     public CharacterDietStats CharacterDietStats { get; private set; }
+    public PersonalInfo PersonalInfo { get; private set; }
+
+    public CharacterTraitContainer[] CharacterTraits { get; private set; }
+    public CharacterProficiencyContainer[] CharacterProficiencies { get; private set; }
 
     public virtual void CopyDataFromCharacterData()
     {
-        if (characterData != null)
+        if (characterData == null)
         {
+            Debug.Log("CharacterData is null in a base class instance");
+            return;
+        }
+        else
+        {
+            InitializeStats();
             CharacterAttributes = new CharacterAttributes(characterData.characterAttributes);
             CharacterStats = new CharacterStats(characterData.characterStats);
             CharacterDietStats = new CharacterDietStats(characterData.characterDietStats);
-            InitializeStats();
+            PersonalInfo = characterData.personalInfo;
+            CharacterTraits = characterData.characterTraitsArray;
+            CharacterProficiencies = characterData.characterProficienciesArray;
         }
+
 
     }
 
