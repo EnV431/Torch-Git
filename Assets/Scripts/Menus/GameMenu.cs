db0d65621300f5ec3eb0f74c80cda3ec838a8c74
sullivaningrid99 <sullivan.ingrid99@gmail.com>
2026-08-05 13:45:14 +0930

    Main Menu Initial Setup
    Added placeholder background and three basic buttons to the main menu scene.
    "Start" button opens the scene called "Game".
    Options has no function currently.
    And "Quit" quits the game.

---
+++ Assets/Scripts/Menus/GameMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
